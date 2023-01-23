c912c27739f8a7e69850b70bb121f1a108e060c1
Toast <76986735+Blixtzz@users.noreply.github.com>
2023-01-22 19:52:35 -0800

    Update AntiEvents.cs

--- ToastModCleaned/QOL/AntiEvents.cs
+++ ToastModCleaned/QOL/AntiEvents.cs
@@ -36,10 +36,6 @@ namespace ToastModCleaned.QOL
                         return false;
                     }
                     break;
-                case 6: 
-                    bytes = __0.customData.Cast<Il2CppArrayBase<byte>>();
-
-                    break;
             }
             return true;
         }
